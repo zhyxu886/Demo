7458a7126a7963a6aef3deae9cf301e6b595c2ed
朱仰旭 <zhyxu88@163.com>
2016-10-14 17:41:34 +0800

    init项目

---
+++ src/main/java/com/ccservice/test/Demo/App.java
@@ -0,0 +1,13 @@
+package com.ccservice.test.Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
